482aeed7cc8c5c053ae6a5a75e1ee1fa7c216cd4
Joe5451 <skybluejoe123@gmail.com>
2025-03-31 22:52:43 +0800

    feat: update user domain
    - add ErrInvalidCredentials and ErrDuplicateEmail
    - add Authenticate method

--- user/internal/domain/user.go
+++ user/internal/domain/user.go
@@ -20,6 +20,8 @@ var (
 	ErrUserEmailCannotBeBlank    = errorx.New(http.StatusBadRequest, "VALIDATION_ERROR", "the user email cannot be blank")
 	ErrUserPasswordCannotBeBlank = errorx.New(http.StatusBadRequest, "VALIDATION_ERROR", "the user password cannot be blank")
 	ErrUserNotAuthorized         = errorx.New(http.StatusUnauthorized, "UNAUTHORIZED", "user is not authorized")
+	ErrInvalidCredentials        = errorx.New(http.StatusUnauthorized, "INVALID_CREDENTIALS", "invalid credentials")
+	ErrDuplicateEmail            = errorx.New(http.StatusConflict, "DUPLICATE_EMAIL", "email already in use")
 )
 
 func RegisterUser(id, name, email, password string) (*User, error) {
@@ -57,4 +59,11 @@ func RegisterUser(id, name, email, password string) (*User, error) {
 func hashPassword(password string) (string, error) {
 	bytes, err := bcrypt.GenerateFromPassword([]byte(password), 10)
 	return string(bytes), err
+}
+
+func (u *User) Authenticate(password string) error {
+	if err := bcrypt.CompareHashAndPassword([]byte(u.Password), []byte(password)); err != nil {
+		return ErrInvalidCredentials
+	}
+	return nil
 }
